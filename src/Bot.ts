19e1a9b7a0efeb561e6c3126b350a0129ccde759
Seth Gaglione <seth.gaglione@live.com>
2020-12-02 14:09:22 -0600

    Added Command Handling

--- src/Bot.ts
+++ src/Bot.ts
@@ -1,22 +1,58 @@
-import {Client, Message} from 'discord.js';
+import {Client} from 'discord.js';
+import {Command} from './abstracts/Command'
+import {Test} from './commands/Test';
 
 export class Bot {
   client: Client;
+  commands: Command[];
+  prefix: string;
 
+  // Make a bot with a client and a collection of commands
   constructor() {
     this.client = new Client();
+    this.commands = [];
+    this.prefix = process.env.PREFIX;
+
+    this.importCommands();
+    this.login();
   }
 
-  async listen() {
+  // Import the commands
+  private importCommands(){
+    this.commands.push(new Test());
+  }
+
+  // Log the bot into the server
+  private async login() {
     await this.client.login(process.env.BOT_TOKEN);
-
     console.log(`Logged in as ${this.client.user.tag}`);
+  } 
 
+  // Listen to the discord server
+  async listen() {
+    // Wait for a message to be sent by a user
     await this.client.on('message', (message) => {
-      if(message.content === 'ping') {
-        message.reply('pong');
+      // Check to see if the command starts with the specified prefix or if the author is the bot
+      if(!message.content.startsWith(this.prefix) || message.author.bot) return;
+
+      //get the arguments from the message
+      const args: string[] = message.content.slice(this.prefix.length).trim().split(/ +/);
+      
+      //Pull the command from the arguments
+      const command: string = args.shift().toLowerCase();
+
+      //Try searching to see if a command is able to be executed
+      try {
+        //Loop through the commands finding triggers and then execute
+        for( var i = 0; i < this.commands.length; i++ ){
+          if(this.commands[i].getTriggers().includes(command)){
+            this.commands[i].execute(message, args);
+          }  
+        }
+      } catch (error: any) {
+        console.error(error);
+        message.reply('Something went wrong while trying to execute that command!');
       }
     });
-
   }
 }
--- src/abstracts/Command.ts
+++ src/abstracts/Command.ts
@@ -3,14 +3,35 @@
 import {Message} from 'discord.js';
 
 export abstract class Command {
-  name: string;
-  description: string;
-  triggers: string[];
+  private name: string;
+  private description: string;
+  private triggers: string[];
 
-  constructor(name: string, description: string) {
+  constructor(
+    name: string, 
+    description: string,
+    triggers: string[]
+  ) {
     this.name = name;
     this.description = description;
+    this.triggers = triggers;
   }
 
+  //Get the Name of the command
+  getName(): string {
+    return this.name;
+  }
+
+  //Get the Description of the command
+  getDescription(): string {
+    return this.description;
+  }
+
+  //Get the Triggers for the command
+  getTriggers(): string[] {
+    return this.triggers;
+  }
+
+  //Abstract execute function for executing command
   abstract execute(message: Message, args: string[]): Promise<void>;
 }
